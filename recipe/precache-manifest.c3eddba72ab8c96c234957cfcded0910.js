b598a533db9e1236ce5ca4c393e7dcd6b3edaef3
yinoue <>
2022-10-10 22:21:43 +0900

    add recipe

---
+++ recipe/precache-manifest.c3eddba72ab8c96c234957cfcded0910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5869eb414d780269c9d1",
+    "url": "/static/css/main.e1b9d3da.chunk.css"
+  },
+  {
+    "revision": "5869eb414d780269c9d1",
+    "url": "/static/js/main.5869eb41.chunk.js"
+  },
+  {
+    "revision": "c625f3bb9e603565083e",
+    "url": "/static/js/1.c625f3bb.chunk.js"
+  },
+  {
+    "revision": "d672b7a483d296b21b42",
+    "url": "/static/js/2.d672b7a4.chunk.js"
+  },
+  {
+    "revision": "c7ddb75be28b480719f2",
+    "url": "/static/js/runtime~main.c7ddb75b.js"
+  },
+  {
+    "revision": "50e71c8e6d3a87609c7ed72f883ac2fc",
+    "url": "/index.html"
+  }
+];
